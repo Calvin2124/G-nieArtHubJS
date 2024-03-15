1bd9b44280eb6c156cd91850ae8eb64056ae1c3a
Calvin Rousseau <calvinrousseau90@gmail.com>
2024-03-15 11:07:40 +0100

    Ajoute les articles Connect l'api au mon script JS Ajoute les données de l'api dans un article

---
+++ front/js/index.js
@@ -0,0 +1,16 @@
+let datas = []
+async function charger_data(){
+    let reponse = await fetch("http://localhost:3000/api/products/")
+    datas = await reponse.json()
+    for (let element of datas){
+        console.log(element)
+        section.insertAdjacentHTML("beforeend", `
+        <article>
+            <img src="${element.image}" alt="${element.titre}">
+            <a href="product.html">Buy ${element.shorttitle}</a>
+        </article>`)
+    }
+}
+const section = document.querySelector(".products")
+charger_data()
+
